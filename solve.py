e11163ca25f4e65858e0611fbd5684b466e49372
Olivia Brode-Roger <nibr@mit.edu>
2019-07-17 11:52:04 -0400

    Logic finished

--- solve.py
+++ solve.py
@@ -6,29 +6,43 @@ class Card:
         # top -> right (clockwise)
         self.right, self.bot, self.left, self.top = colors
 
-def solve(cards, board = [], pos = 0):
-    for c in cards:
+EMPTY_BOARD = [None for _ in range(9)]
+def solve(cards, board = EMPTY_BOARD, pos = 0):
+    for i, c in enumerate(cards):
         # place card
         if place(board, c, pos):
-            if solve(cards, board, pos + 1):
+            remaining_cards = cards[:i] + cards[i+1:]
+            # recurse
+            if solve(remaining_cards, board, pos + 1):
+                # if success, return
                 return True
-        # recurse
-        #  if fail, undo, do next
-        #  if success, return
-    #failed
+        # if we're here, we failed, undo, do next
+        board[pos] = None
+
+    # None worked, we failed
     return False
 
 def place(board, card, pos):
     row, col = pos_to_rc(pos)
     for _ in range(4):
-        fits = False
+        fits = True
         if row > 0:
-            # check
+            above = rc_to_pos(row-1, col)
+            if above.bot != card.top:
+                fits = False
         if col > 0:
             # check
+            left = rc_to_pos(row, col-1)
+            if left.right != card.left:
+                fits = False
 
         if fits:
+            board[pos] = card
             return True
+        else:
+            card.rotate()
+
+    # None fit, we fail...
     return False
 
 def rc_to_pos(row, col):
