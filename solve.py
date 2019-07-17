f54e50e47a8402e4324a8da0ce863d2c276b97d2
Olivia Brode-Roger <nibr@mit.edu>
2019-07-17 12:42:22 -0400

    working with colors!

--- solve.py
+++ solve.py
@@ -1,10 +1,16 @@
 class Card:
     def __init__(self, colors):
         self.top, self.right, self.bot, self.left = colors
+        self.colors = colors
 
     def rotate(self):
         # top -> right (clockwise)
-        self.right, self.bot, self.left, self.top = colors
+         self.right, self.bot, self.left, self.top = (
+           self.top, self.right, self.bot, self.left)
+
+    def __str__(self):
+        colors = [self.top, self.right, self.bot, self.left]
+        return str(colors)
 
 WIDTH  = 2
 HEIGHT = 2
@@ -12,44 +18,50 @@ EMPTY_BOARD = [None for _ in range(WIDTH*HEIGHT)]
 
 
 def solve(cards, board = EMPTY_BOARD, pos = 0):
+    print([str(c) for c in board])
+    if len(cards) == 0:
+        return True, board
+
     for i, c in enumerate(cards):
         # place card
-        if place(board, c, pos):
-            remaining_cards = cards[:i] + cards[i+1:]
-            # recurse
-            if solve(remaining_cards, board, pos + 1):
-                # if success, return
-                return True
-        # if we're here, we failed, undo, do next
-        board[pos] = None
+        for _ in range(5):
+            placed, board = place(board, c, pos)
+            if placed:
+                remaining_cards = cards[:i] + cards[i+1:]
+                # recurse
+                solved, board = solve(remaining_cards, board, pos + 1)
+                if solved:
+                    # if success, return
+                    return True, board
+                c.rotate()
+
+            # if we're here, we failed, undo, do next
+            board[pos] = None
 
     # None worked, we failed
-    return False
+    board[pos] = None
+    return False, board
 
 def place(board, card, pos):
     row, col = pos_to_rc(pos)
-    for _ in range(4):
-        fits = True
-        if row > 0:
-            above_pos = rc_to_pos(row-1, col)
-            above = board[pos]
-            if above.bot != card.top:
-                fits = False
-        if col > 0:
-            # check
-            left_pos = rc_to_pos(row, col-1)
-            left = boatd[pos]
-            if left.right != card.left:
-                fits = False
 
-        if fits:
-            board[pos] = card
-            return True
-        else:
-            card.rotate()
+    fits = True
+    if row > 0:
+        above_pos = rc_to_pos(row-1, col)
+        above = board[above_pos]
+        if above.bot != card.top:
+            fits = False
+    if col > 0:
+        # check
+        left_pos = rc_to_pos(row, col-1)
+        left = board[left_pos]
+        if left.right != card.left:
+            fits = False
 
-    # None fit, we fail...
-    return False
+    if fits:
+        board[pos] = card
+
+    return fits, board
 
 def rc_to_pos(row, col):
     return WIDTH*row + col
@@ -67,5 +79,11 @@ colors = [
         ]
 
 cards = [Card(cols) for cols in colors]
+for c in cards:
+    print(c)
+print("")
+
 board = EMPTY_BOARD
-solve(cards, board)
+s, board = solve(cards, board)
+print(s)
+print([str(c) for c in board])
