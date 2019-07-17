392d7846f23746d191d72b222eac14f64498c69f
Olivia Brode-Roger <nibr@mit.edu>
2019-07-17 14:30:31 -0400

    Added directions

--- solve.py
+++ solve.py
@@ -1,12 +1,41 @@
 class Card:
-    def __init__(self, colors):
+    def __init__(self, colors, dirs):
         self.top, self.right, self.bot, self.left = colors
-        self.colors = colors
+
+        self.top_dir, self.right_dir, self.bot_dir, self.left_dir = dirs
 
     def rotate(self):
         # top -> right (clockwise)
          self.right, self.bot, self.left, self.top = (
            self.top, self.right, self.bot, self.left)
+
+         self.right_dir, self.bot_dir, self.left_dir, self.top = (
+           self.top_dir, self.right_dir, self.bot_dir, self.left)
+
+
+
+    def __str__(self):
+        colors = [self.top, self.right, self.bot, self.left]
+        return str(colors)
+
+WIDTH  = 2
+HEIGHT = 2
+EMPTY_BOARD = [None for _ in range(WIDTH*HEIGHT)]
+
+
+def solve(cards, board = EMPTY_BOARD, pos = 0):
+    print([str(c) for c in board])
+    if len(cards) == 0:
+        return True, board
+
+    for i, c in enumerate(cards):
+        # place card
+        for _ in range(4):
+            if place(board, c, pos):
+         self.right, self.bot, self.left, self.top = (
+           self.top, self.right, self.bot, self.left)
+
+
 
     def __str__(self):
         colors = [self.top, self.right, self.bot, self.left]
@@ -49,13 +78,15 @@ def place(board, card, pos):
     if row > 0:
         above_pos = rc_to_pos(row-1, col)
         above = board[above_pos]
-        if above.bot != card.top:
+        if above.bot     != card.top or
+           above.bot_dir == card.top_dir:
             fits = False
     if col > 0:
         # check
         left_pos = rc_to_pos(row, col-1)
         left = board[left_pos]
-        if left.right != card.left:
+        if left.right     != card.left or
+           left.right_dir == card.left_dir:
             fits = False
 
     return fits
@@ -75,7 +106,14 @@ colors = [
         ['or', 'or', 'bl', 'gr'],
         ]
 
-cards = [Card(cols) for cols in colors]
+dirs = [
+        ['o',  'i',  'i',  'o'],
+        ['i',  'i',  'o',  'o'],
+        ['i',  'o',  'o',  'i'],
+        ['i',  'o',  'o',  'i'],
+        ]
+
+cards = [Card(cols, dirs) for cols, dirs in zip(colors, dirs)]
 for c in cards:
     print(c)
 print("")
